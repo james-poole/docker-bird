c376e074adcf70f45ce3da99758ab840a9bb9de5
James Poole <james.poole@james.poole@fasthosts.com>
2018-07-15 17:45:51 +0100

    Add readiness before starting bird

--- files/init/entrypoint.sh
+++ files/init/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -eu
 
-for inf in $(find /tmp/loopback-aliases/ -type f -name "ifcfg-lo.[0-9]*" -printf "%f\n") ; do
-  inf_alias=$(echo $inf | sec 's/\./:/g')
-  cp /tmp/loopback-aliases/$inf /etc/sysconfig/network-scripts/$inf_alias
-  echo "Starting $inf_alias"
-  ifup $inf_alias
-done
+RET=$(/bin/sh /usr/local/bin/bgp-healthcheck.sh &>/dev/null ; echo $?)
 
-/usr/sbin/bird -c /etc/bird.conf -f
+if [ $RET == "0" ]; then
+  for inf in $(find /tmp/loopback-aliases/ -type f -name "ifcfg-lo.[0-9]*" -printf "%f\n") ; do
+    inf_alias=$(echo $inf | sec 's/\./:/g')
+    cp /tmp/loopback-aliases/$inf /etc/sysconfig/network-scripts/$inf_alias
+    echo "Starting $inf_alias"
+    ifup $inf_alias
+  done
+  /usr/sbin/bird -c /etc/bird.conf -f
+else
+  exit 1
+fi
