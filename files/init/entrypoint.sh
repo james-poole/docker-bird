0ab3b2475a8e63771bb8cee13349bc13d8e20526
James Poole <james.poole@james.poole@fasthosts.com>
2018-07-15 16:25:53 +0100

    init script tweak to work with configmap

--- files/init/entrypoint.sh
+++ files/init/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -eu
 
-for inf in $(find /etc/sysconfig/network-scripts/ -type f -name "ifcfg-lo\:[0-9]*" -printf "%f\n") ; do
-  echo "Starting $inf"
-  ifup $inf
+for inf in $(find /tmp/loopback-aliases/ -type f -name "ifcfg-lo.[0-9]*" -printf "%f\n") ; do
+  inf_alias=$(echo $inf | sec 's/\./:/g')
+  cp /tmp/loopback-aliases/$inf /etc/sysconfig/network-scripts/$inf_alias
+  echo "Starting $inf_alias"
+  ifup $inf_alias
 done
 
 /usr/sbin/bird -c /etc/bird.conf -f
