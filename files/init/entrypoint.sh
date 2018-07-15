2a3b717e7b7b3074c05cfd5163d756ddba901081
James Poole <james.poole@james.poole@fasthosts.com>
2018-07-15 18:14:07 +0100

    Typo in init script

--- files/init/entrypoint.sh
+++ files/init/entrypoint.sh
@@ -5,7 +5,7 @@ RET=$(/bin/sh /usr/local/bin/bgp-healthcheck.sh &>/dev/null ; echo $?)
 
 if [ $RET == "0" ]; then
   for inf in $(find /tmp/loopback-aliases/ -type f -name "ifcfg-lo.[0-9]*" -printf "%f\n") ; do
-    inf_alias=$(echo $inf | sec 's/\./:/g')
+    inf_alias=$(echo $inf | sed 's/\./:/g')
     cp /tmp/loopback-aliases/$inf /etc/sysconfig/network-scripts/$inf_alias
     echo "Starting $inf_alias"
     ifup $inf_alias
