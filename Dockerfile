f45afa94f983fdc66ccc49f365e17b11043d57a9
James Poole <james.poole@james.poole@fasthosts.com>
2018-07-16 13:41:00 +0100

    Stop managing host network interfaces via docker, we'll use config management (puppet or ansible tower) to ensure the adequate loop back addresses are present.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM docker.io/fedora:28
 COPY files/ /
 RUN \
-  dnf install bird iproute initscripts -y && \
+  dnf install bird -y && \
   dnf clean all && \
   rm -rf /var/cache/dnf && \
   chmod 0700 /init/entrypoint.sh
--- files/init/entrypoint.sh
+++ files/init/entrypoint.sh
@@ -4,12 +4,6 @@ set -eu
 RET=$(/bin/sh /usr/local/bin/bgp-healthcheck.sh &>/dev/null ; echo $?)
 
 if [ $RET == "0" ]; then
-  for inf in $(find /tmp/loopback-aliases/ -type f -name "ifcfg-lo.[0-9]*" -printf "%f\n") ; do
-    inf_alias=$(echo $inf | sed 's/\./:/g')
-    cp /tmp/loopback-aliases/$inf /etc/sysconfig/network-scripts/$inf_alias
-    echo "Starting $inf_alias"
-    ifup $inf_alias
-  done
   /usr/sbin/bird -c /etc/bird.conf -f
 else
   exit 1
