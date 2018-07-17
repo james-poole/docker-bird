b3c8a765f84363827411cd8d7061f08ca5a2b3a7
James Poole <james.poole@james.poole@fasthosts.com>
2018-07-17 07:27:50 +0100

    Manage bird state within container.

--- files/init/entrypoint.sh
+++ files/init/entrypoint.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 set -eu
 
-echo "Running health check before starting bird:"
-RET=$(/bin/sh /usr/local/bin/bgp-healthcheck.sh &>/dev/null ; echo $?)
+function cleanup {
+  echo "Received exit code, stopping bird if it's running."
+  [ $(pgrep -f '/usr/sbin/bird -c /etc/bird.conf') ] && birdc down
+}
 
-if [ $RET == "0" ]; then
-  echo "Health check passed, starting bird."
-  /usr/sbin/bird -c /etc/bird.conf -f
-else
-  echo "Health check failed, dieing."
-  exit 1
-fi
+trap cleanup EXIT
+
+echo "Running health check before starting bird:"
+
+while true; do
+  RET=$(/bin/sh /usr/local/bin/bgp-healthcheck.sh &>/dev/null ; echo $?)
+  INTERVAL=${BGP_CHECK_INTERVAL:-5s}
+  if [ $RET == "0" ]; then
+    if [ $(pgrep -f '/usr/sbin/bird -c /etc/bird.conf') ]; then
+      echo "Health check passed and bird is running."
+    else
+      echo "Health check passed, starting bird."
+      /usr/sbin/bird -c /etc/bird.conf
+    fi
+  else
+    if [ $(pgrep -f '/usr/sbin/bird -c /etc/bird.conf') ]; then
+      echo "Health check failed and bird is running, stopping bird."
+      birdc down
+    else
+      echo "Health check failed and bird is down."
+    fi
+  fi
+  sleep $INTERVAL
+done
+
