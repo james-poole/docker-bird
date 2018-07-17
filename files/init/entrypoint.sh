288bde04e05801cfe7e8c02dd72f16ab3d22f895
James Poole <james.poole@james.poole@fasthosts.com>
2018-07-17 14:12:45 +0100

    Added dates to log entries.

--- files/init/entrypoint.sh
+++ files/init/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function cleanup {
-  echo "Received exit code, stopping bird if it's running."
+  echo "$(date): Received exit code, stopping bird if it's running."
   if [ $(pgrep -f '/usr/sbin/bird -c /etc/bird.conf') ] ; then
     birdc down
     exit 0
@@ -11,24 +11,24 @@ function cleanup {
 
 trap cleanup EXIT HUP INT QUIT PIPE TERM
 
-echo "Running health check before starting bird:"
+echo "$(date): Running health check before starting bird:"
 
 while true; do
   RET=$(/bin/sh /usr/local/bin/bgp-healthcheck.sh &>/dev/null ; echo $?)
   INTERVAL=${BGP_CHECK_INTERVAL:-5s}
   if [ $RET == "0" ]; then
     if [ $(pgrep -f '/usr/sbin/bird -c /etc/bird.conf') ]; then
-      echo "Health check passed and bird is running."
+      echo "$(date): Health check passed and bird is running."
     else
-      echo "Health check passed, starting bird."
+      echo "$(date): Health check passed, starting bird."
       /usr/sbin/bird -c /etc/bird.conf
     fi
   else
     if [ $(pgrep -f '/usr/sbin/bird -c /etc/bird.conf') ]; then
-      echo "Health check failed and bird is running, stopping bird."
+      echo "$(date): Health check failed and bird is running, stopping bird."
       birdc down
     else
-      echo "Health check failed and bird is down."
+      echo "$(date): Health check failed and bird is down."
     fi
   fi
   sleep $INTERVAL
